f6cbbd0bcb3646a30772a1739450af447ba123a1
subairifeoluwapo <subairifeoluwapo@gmail.com>
2014-09-05 12:55:55 +0100

    updated 12:55

--- jquery/myscript.js
+++ jquery/myscript.js
@@ -1,4 +1,17 @@
 
+$('.beanstalkimage').fadeIn("slow")
+
 $('.gameName').hide().show("slow");
 
-$('.')
+$('.enterNumber')
+
+$('.canYouGuess')
+
+$('enterNumber').click(function() {
+	prompt("Are you ready to play?")
+})
+
+
+$('.inputNumber')
+
+$(".enterNumber").Math.random()
