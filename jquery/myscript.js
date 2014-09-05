31b84d8e61da4b31cc0c38c1d1f7bac789ac209d
subairifeoluwapo <subairifeoluwapo@gmail.com>
2014-09-05 14:59:01 +0100

    updated 14:48

--- jquery/myscript.js
+++ jquery/myscript.js
@@ -1,15 +1,21 @@
+var randomNumber = Math.floor(Math.random() * 100);
 
 $('.beanstalkimage').fadeIn("slow");
 
 $('.gameName').hide().show("slow");
 
 
-var randomNumber = Math.floor(Math.random() % 1);
+// Grab the guess from the text input field
+    var guess = $('.enteredNumber').val();
 
 
-$( "input[type='number']" ).on( "blur", function() {
-if(!this.value ) {
-alert( "Please enter a number between 0 and 100!" );
+// Checks to see if the guess is within the parameters given
+function validGuess(guess) {
+    return isNormalInteger(guess) && +guess <= 100 && +guess >= 0;
 }
-});
 
+if (validGuess(guess)) {
+	$('.wrong')
+
+ // Convert guess value to an integer for comparison
+        guess = parseInt(guess, 10);
