266283e3ebf0cf8f9735ee767ec90a4860b6a2aa
subairifeoluwapo <subairifeoluwapo@gmail.com>
2014-09-08 16:07:24 +0100

    updated

--- js/myscript.js
+++ js/myscript.js
@@ -2,7 +2,6 @@ $('.beanstalkimage').fadeIn("slow");
 
 $('.gameName').hide().show("slow");
 
-
 // // Global variable//
 var randomNumber = Math.floor(Math.random() * 100);
 
@@ -11,16 +10,14 @@ var guess;
 
 // // Checks to see if the guess is within the parameters given
 
-
 var validEntry = function(guess) {
 	return (guess <= 100 && guess >= 0);
 }
 
 var compareEntry = function() {
-	//event.preventDefault();
-	
 	// // Grab the guess from the text input field
     guess = $('#enteredNumber').val();
+
     if (guess === '' || guess === ' ' || guess === 'string') {
         $('#invalidEntry').append('Invalid Entry! Enter a number between 0 and 100.')
     }
@@ -29,55 +26,43 @@ var compareEntry = function() {
 
         // Convert guess value to an integer for comparison
     	guess = parseInt(guess, 10);
-
-        if (guess === randomNumber) {
-            $('#enteredNumber-vs-number').text('You got it! The number was ' + randomNumber + '.');
-          	$('#enteredNumber-vs-guess').hide();
-
- 	    } 	
-            else if (guess > randomNumber) {
-                $('#enteredNumber-vs-number').text('Higher than number');
-            }
-            else if (guess < randomNumber) {
-                $('#enteredNumber-vs-number').text('Lower than number');
-            }
-
-        
- 	    // Blank out the guess input field and return focus to it
-         $('#enteredNumber').val('').focus();
-
-     	if (guess) {
-          // Find distances of the current and previous guesses from the actual number
-        	var previousDistance = Math.abs(randomNumber - previousEntry);
-        	var currentDistance = Math.abs(randomNumber - guess);
-
-         // Feedback for guess versus previous guess comparison
-           if (guess === previousEntry) {
-	           $('#enteredNumber-vs-guess').text("Same guess!");
-	        } else if (currentDistance < previousDistance){
-	            $('#enteredNumber-vs-guess').text("Getting Hotter...");
-	        } else if (currentDistance > previousDistance) {
-	            $('#enteredNumber-vs-guess').text("Getting colder...");
-	        } else {
-	            $('#enteredNumber-vs-guess').text("Same distance...");
-	        }
-                previousEntry = guess;
-	 		}    
-
-	 	}
-			     else {
-	 		        //Blank out the guess field and return focus.
-		        $('#enteredNumber').val('').focus();
-	 		    }
+	        if (guess === randomNumber) {
+	            $('#enteredNumber-vs-number').text('You got it! The number was ' + randomNumber + '.');
+	 	    } 	
+	            else if (Math.abs(guess - randomNumber) <= 5) {
+	                $('#enteredNumber-vs-number').text('You are very hot!');
+	            }
+	            else if (Math.abs(guess - randomNumber)  <= 10) {
+	                $('#enteredNumber-vs-number').text('You are hot!');
+	            }
+	        	else if (Math.abs(guess - randomNumber)  >= 10 && Math.abs(guess - randomNumber) <= 20) {
+	                $('#enteredNumber-vs-number').text('You are warm!');
+	            }
+	            else if (Math.abs(guess - randomNumber)  >= 20 && Math.abs(guess - randomNumber) <= 30) {
+	                $('#enteredNumber-vs-number').text('You are cold!');
+	            }
+	            else if (Math.abs(guess - randomNumber)  >= 30 && Math.abs(guess - randomNumber) <= 40) {
+	                $('#enteredNumber-vs-number').text('You are very cold!');
+	            }
+	            else {
+	                $('#enteredNumber-vs-number').text('You are freezing cold!');
+	            }
+	 	    // Blank out the guess input field and return focus to it
+	         $('#enteredNumber').val('').focus();
+		}    
+				    else {
+		 		        //Blank out the guess field and return focus.
+			       		$('#enteredNumber').val('').focus();
+		 		    }
 }
 
-// // Bind a click of the reset button to browser reload
+// Bind a click of the reset button to browser reload
 $('#entryForm').on('click', '#restart', function() {
 	event.preventDefault();
 	location.reload();
 	 });
 
-// // Bind form submission to the compareEntry function
+// Bind form submission to the compareEntry function
 $('#entryForm').on('click', '#submitbutton', function(){
 	compareEntry();
-});//compareEntry(event));
+});
