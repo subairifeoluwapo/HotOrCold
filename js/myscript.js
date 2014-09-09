cb2b1772a336ee553e8e9406146d13ee911b6d76
subairifeoluwapo <subairifeoluwapo@gmail.com>
2014-09-09 14:02:25 +0100

    update

--- js/myscript.js
+++ js/myscript.js
@@ -2,8 +2,11 @@
 
 
 var HotOrColdGame = {
-	// Global variable//
-	
+
+	randomNumber: function() {
+		randomNumber = Math.floor(Math.random() * 100);
+	},
+
 	animate: function() {
 		$('.beanstalkimage').fadeIn("slow");
 		$('.gameName').hide().show("slow");
@@ -17,32 +20,32 @@ var HotOrColdGame = {
 	},
 
 	compareEntry: function() {
+		
 		if (HotOrColdGame.validEntry()) {
 			$('#invalidEntry').remove();
-			var randomNumber = Math.floor(Math.random() * 100);
 			var guess = $('#enteredNumber').val();
 	        // Convert guess value to an integer for comparison
 	    	guess = parseInt(guess, 10);
 	        if (guess === randomNumber) {
-	            $('#enteredNumber-vs-number').text('You got it! The number was ' + randomNumber + '.');
+	            $('#enteredNumber-vs-number').text('You got it Genius! The number was ' + randomNumber + '.');
 	 	    } 	
 	            else if (Math.abs(guess - randomNumber) <= 5) {
-	                $('#enteredNumber-vs-number').text('You are very hot!');
+	                $('#enteredNumber-vs-number').text('You are hotter than the Mecury!');
 	            }
 	            else if (Math.abs(guess - randomNumber)  <= 10) {
-	                $('#enteredNumber-vs-number').text('You are hot!');
+	                $('#enteredNumber-vs-number').text('You are hot');
 	            }
 	        	else if (Math.abs(guess - randomNumber)  >= 10 && Math.abs(guess - randomNumber) <= 20) {
-	                $('#enteredNumber-vs-number').text('You are warm!');
+	                $('#enteredNumber-vs-number').text('You are warm...Global-Warming has nothing on you !');
 	            }
 	            else if (Math.abs(guess - randomNumber)  >= 20 && Math.abs(guess - randomNumber) <= 30) {
 	                $('#enteredNumber-vs-number').text('You are cold!');
 	            }
 	            else if (Math.abs(guess - randomNumber)  >= 30 && Math.abs(guess - randomNumber) <= 40) {
-	                $('#enteredNumber-vs-number').text('You are very cold!');
+	                $('#enteredNumber-vs-number').text('You are as cold as Neptune!');
 	            }
 	            else {
-	                $('#enteredNumber-vs-number').text('You are freezing cold!');
+	                $('#enteredNumber-vs-number').text('You are so you could freeze the ocean!');
 	            }
 	 	    // Blank out the guess input field and return focus to it
 	         $('#enteredNumber').val('').focus();
@@ -66,5 +69,6 @@ var HotOrColdGame = {
 	},
 
 }
+$(document).ready(HotOrColdGame.randomNumber);
 $(document).ready(HotOrColdGame.animate);
 $(document).ready(HotOrColdGame.initialize);
